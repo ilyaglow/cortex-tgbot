d93a78e161fa93aacb8bade49067be14b53d2581
Ilya Glotov <ilya.glotov@gmail.com>
2017-10-23 17:38:52 +0300

    Fix typo in a project name

---
+++ cmd/irbot/main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"log"
+
+	"github.com/ilyaglow/irbot"
+	"gopkg.in/telegram-bot-api.v4"
+)
+
+func main() {
+	c := irbot.NewClient()
+	c.Bot.Debug = true
+	log.Printf("Authorized on account %s", c.Bot.Self.UserName)
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates, err := c.Bot.GetUpdatesChan(u)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for update := range updates {
+
+		if update.CallbackQuery != nil {
+			go c.ProcessCallback(update.CallbackQuery)
+			continue
+		}
+
+		if update.Message == nil {
+			continue
+		}
+
+		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+		if update.Message.IsCommand() {
+			go c.ProcessCommand(update.Message)
+			continue
+		}
+	}
+}
