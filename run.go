4b6fddbb6d3729b76b20a776853682ffa0769c8e
Ilya Glotov <ilya.glotov@gmail.com>
2017-10-27 21:27:33 +0300

    Move main logic to the package level

---
+++ run.go
@@ -0,0 +1,40 @@
+package cortexbot
+
+import (
+	"log"
+
+	"gopkg.in/telegram-bot-api.v4"
+)
+
+func (c *Client) Run() {
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+
+	updates, err := c.Bot.GetUpdatesChan(u)
+	if err != nil {
+		log.Fatal(err)
+	}
+	log.Printf("Authorized on account %s", c.Bot.Self.UserName)
+
+	for update := range updates {
+		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+		msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
+		msg.ReplyToMessageID = update.Message.MessageID
+
+		if update.Message.IsCommand() && update.Message.Command() == "start" {
+			msg.Text = "Enter your password"
+			c.Bot.Send(msg)
+			continue
+		}
+
+		if c.CheckAuth(update.Message.From.UserName) {
+			if err := c.ProcessCortex(update.Message); err != nil {
+				msg.Text = err.Error()
+				c.Bot.Send(msg)
+			}
+		} else {
+			c.Auth(update.Message)
+		}
+	}
+}
