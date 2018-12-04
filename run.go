538c119f5f6f43811c77c8c9ef28a3656de6633a
Ilya Glotov <contact@ilyaglotov.com>
2018-12-05 00:16:24 +0300

    Run goroutine on every update
    Additionally, cleanup and simplify code to process tgbotapi.Update

--- run.go
+++ run.go
@@ -25,47 +25,10 @@ func (c *Client) Run() {
 	log.Printf("Users in database: %s", strings.Join(c.listUsers(), ","))
 
 	for update := range updates {
-
-		if update.CallbackQuery != nil {
-			log.Printf("username: %s, id: %d, text: %s", update.CallbackQuery.Message.From.UserName, update.CallbackQuery.Message.From.ID, update.CallbackQuery.Message.Text)
-			go func() {
-				if err := c.processCallback(update.CallbackQuery); err != nil {
-					log.Println(err)
-				}
-				cbcfg := tgbotapi.NewCallback(update.CallbackQuery.ID, "")
-				if _, err := c.Bot.AnswerCallbackQuery(cbcfg); err != nil {
-					log.Println(err)
-				}
-			}()
-		} else {
-			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
-			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "")
-			msg.ReplyToMessageID = update.Message.MessageID
-
-			if update.Message.IsCommand() &&
-				update.Message.Command() == "start" &&
-				!c.CheckAuth(update.Message.From) {
-				c.log("run: new unauthorized user")
-				msg.Text = "Enter your password"
-				if _, err := c.Bot.Send(msg); err != nil {
-					log.Println(err)
-				}
-				continue
+		go func(upd *tgbotapi.Update) {
+			if err := c.process(upd); err != nil {
+				log.Println(err)
 			}
-
-			if c.CheckAuth(update.Message.From) {
-				c.log("run: auth succeeded")
-				go func() {
-					if err := c.processMessage(update.Message); err != nil {
-						log.Println(err)
-					}
-				}()
-			} else {
-				c.log("run: wrong password from the user")
-				if err := c.Auth(update.Message); err != nil {
-					log.Println(err)
-				}
-			}
-		}
+		}(&update)
 	}
 }
