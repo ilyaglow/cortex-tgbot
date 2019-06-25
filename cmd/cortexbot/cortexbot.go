037e54075257a13ee7dcfa8f976c197684fc0213
Ilya Glotov <contact@ilyaglotov.com>
2019-06-25 19:23:03 +0300

    Add reasonable http timeouts

--- cmd/cortexbot/cortexbot.go
+++ cmd/cortexbot/cortexbot.go
@@ -3,11 +3,15 @@ package main
 import (
 	"context"
 	"log"
+	"net"
+	"net/http"
 	"os"
 	"os/signal"
 	"syscall"
+	"time"
 
 	"github.com/ilyaglow/cortex-tgbot"
+	cortex "github.com/ilyaglow/go-cortex"
 )
 
 func main() {
@@ -16,6 +20,26 @@ func main() {
 		log.Fatal(err)
 	}
 	c.Bot.Debug = true
+
+	crtx, err := cortex.NewClient(os.Getenv("CORTEX_URL"), &cortex.ClientOpts{
+		Auth: &cortex.APIAuth{
+			APIKey: os.Getenv("CORTEX_API_KEY"),
+		},
+		HTTPClient: &http.Client{
+			Timeout: time.Second * 10,
+			Transport: &http.Transport{
+				Dial: (&net.Dialer{
+					Timeout: 5 * time.Second,
+				}).Dial,
+				TLSHandshakeTimeout: 5 * time.Second,
+			},
+		},
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	c.Cortex = crtx
 
 	ctx, cancel := context.WithCancel(context.Background())
 	ctrlc := make(chan os.Signal, 1)
--- go.mod
+++ go.mod
@@ -1,9 +1,9 @@
 module github.com/ilyaglow/cortex-tgbot
 
 require (
-	github.com/asaskevich/govalidator v0.0.0-20180720115003-f9ffefc3facf
+	github.com/asaskevich/govalidator v0.0.0-20190424111038-f61b66f89f4a
 	github.com/go-telegram-bot-api/telegram-bot-api v4.6.4+incompatible
-	github.com/ilyaglow/go-cortex/v3 v3.0.2
+	github.com/ilyaglow/go-cortex/v3 v3.1.0
 	github.com/mattn/go-sqlite3 v1.10.0
 	github.com/technoweenie/multipartstreamer v1.0.1 // indirect
 	golang.org/x/net v0.0.0-20190322120337-addf6b3196f6
